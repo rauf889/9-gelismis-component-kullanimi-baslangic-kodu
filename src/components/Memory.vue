bf69633cec1455ad111cdead5af69e7ec5d4f1f3
rauf889 <ryagubov889@gmail.com>
2021-10-12 08:27:39 +0300

    new

---
+++ src/components/Memory.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+    <div class= "memory">
+      <slot name="heading"></slot> 
+    </div>
+     <div class= "memory">
+      <slot ></slot> 
+    </div>
+
+    <div class="memory">
+      <slot name="author"></slot>
+      <h3>Author</h3>
+      <h3>Buraya herhangi bi veri gelmemis</h3>
+    </div>
+    
+    
+    
+  </div>
+</template>
+
+
+<script>
+export default{
+
+}
+
+</script>
+
+
+<style scoped>
+  div.memory {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px #666;
+    padding: 20px 20px;
+    margin-top: 20px;
+  }
+</style>
+
+
---
+++ src/components/NewMemory.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h1>Yeni Ani | componenti</h1>
+      <button @click="counter++">Add</button>
+      <p>{{ counter }}</p>
+  </div>
+</template>
+
+
+<script>
+export default{
+    data:function(){
+        return{
+            counter:0
+        }
+    },
+    destroyed(){
+        console.log("Destroyed!!!");
+    },
+    activated(){
+        console.log("Activated!!!")
+
+    },
+    deactivated(){
+        console.log("Deactivated!!!")
+    }
+
+}
+</script>
